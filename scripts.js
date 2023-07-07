613ebe2e8e128d71e3c8ec8bf9933aa553268422
Obyno003 <ogbonnaemmanuel003@gmail.com>
2023-07-07 15:59:35 +0100

    Added responsive clicks to the hamburger icon

--- scripts.js
+++ scripts.js
@@ -1,7 +1,22 @@
-const hamburger= document.querySelector('.hamburger')
-const hamburgerMenu= document.querySelector('.hamburger-menu');
-const closeMenu= document.querySelector ('.close-menu')
+const hamburgerIcon = document.querySelector ('.hamburger-icon')
+const closeIcon = document.querySelector ('.close-icon')
+const hamburgerMenu = document.querySelector ('.hamburger-menu')
+const header = document.querySelector ('.position')
 
-hamburger.addEventListener('click', ()=> {hamburgerMenu.classList.toggle('visible')}
+hamburgerIcon.addEventListener('click', ()=> {
+    hamburgerIcon.classList.add('hide');
+    closeIcon.classList.remove('hide');
+    hamburgerMenu.classList.toggle('visible');
+    header.classList.add('hide');
+}
 )
-hamburger.addEventListener ('click', => closeMenu.classList.toggle('visible'))
+
+closeIcon.addEventListener('click', ()=> {
+    closeIcon.classList.add('hide');
+    hamburgerIcon.classList.remove('hide');
+    hamburgerMenu.classList.toggle('visible');
+    header.classList.remove('hide');
+}
+)
+
+/*search for and install "ScrnLi extention" on chrome to be able to record on chrome.*/
